4ca24259aba768c11c0fccca4fd0fec4d962f388
Czechthisout <ajcech23@gmail.com>
2023-04-21 21:22:59 -0700

    added hide option for the sidebar

---
+++ admin_portal.js
@@ -0,0 +1,15 @@
+window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("toggle").addEventListener("click", () => {
+      const sidebarEl = document.getElementsByClassName("sidebar")[0];
+      const containerEl = document.getElementsByClassName("container")[0];
+      sidebarEl.classList.toggle("sidebar--isHidden");
+      containerEl.classList.toggle("container--sidebarHidden");
+  
+      // Update the toggle button position
+      const toggleButtonEl = document.getElementById("toggle");
+      toggleButtonEl.style.left = containerEl.classList.contains("container--sidebarHidden")
+        ? "0"
+        : "12rem";
+    });
+  });
+  
