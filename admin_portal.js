93906cf7f433c558c63de619ce7e50ebec467b34
Czechthisout <ajcech23@gmail.com>
2023-04-23 17:12:40 -0700

    added a  class, color pallet, and shadow pallet

--- admin_portal.js
+++ admin_portal.js
@@ -1,15 +1,42 @@
-window.addEventListener("DOMContentLoaded", () => {
+// window.addEventListener("DOMContentLoaded", () => {
+//     document.getElementById("toggle").addEventListener("click", () => {
+//       const sidebarEl = document.getElementsByClassName("sidebar")[0];
+//       const containerEl = document.getElementsByClassName("container")[0];
+//       sidebarEl.classList.toggle("sidebar--isHidden");
+//       containerEl.classList.toggle("container--sidebarHidden");
+  
+//       Update the toggle button position
+//       const toggleButtonEl = document.getElementById("toggle");
+//       toggleButtonEl.style.left = containerEl.classList.contains("container--sidebarHidden")
+//         ? "0"
+//         : "12rem";
+//     });
+//   });
+  
+  // window.addEventListener("DOMContentLoaded", () => {
+  //   document.getElementById("toggle").addEventListener("click", () => {
+  //     const sidebarEl = document.getElementsByClassName("sidebar")[0];
+  //     const containerEl = document.getElementsByClassName("container")[0];
+  //     sidebarEl.classList.toggle("sidebar--isHidden");
+  //     containerEl.classList.toggle("container--sidebarHidden");
+  
+  //     // Update the toggle button position
+  //     const toggleButtonEl = document.getElementById("toggle");
+  //     toggleButtonEl.style.left = containerEl.classList.contains("container--sidebarHidden")
+  //       ? "0"
+  //       : "-2.2rem";
+  //   });
+  // });
+
+  window.addEventListener("DOMContentLoaded", () => {
     document.getElementById("toggle").addEventListener("click", () => {
       const sidebarEl = document.getElementsByClassName("sidebar")[0];
       const containerEl = document.getElementsByClassName("container")[0];
       sidebarEl.classList.toggle("sidebar--isHidden");
       containerEl.classList.toggle("container--sidebarHidden");
   
-      // Update the toggle button position
-      const toggleButtonEl = document.getElementById("toggle");
-      toggleButtonEl.style.left = containerEl.classList.contains("container--sidebarHidden")
-        ? "0"
-        : "12rem";
+      // No need to update the toggle button position
     });
   });
+  
   
