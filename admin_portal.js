e42892bd7d11ba95b23b6e4f242576a7ad6f4784
Czechthisout <ajcech23@gmail.com>
2023-04-25 09:37:30 -0700

    fixed sizing of the Github calendar

--- admin_portal.js
+++ admin_portal.js
@@ -9,4 +9,22 @@
     });
   });
   
+  const sidebar = document.querySelector('.sidebar');
+  const resizer = document.querySelector('.sidebar-resizer');
+  let isResizing = false;
   
+  resizer.addEventListener('mousedown', (e) => {
+    isResizing = true;
+    e.preventDefault();
+  });
+  
+  document.addEventListener('mousemove', (e) => {
+    if (!isResizing) return;
+    const minWidth = 60; // Set the minimum width of the sidebar
+    const newWidth = Math.max(minWidth, e.clientX - sidebar.getBoundingClientRect().left);
+    sidebar.style.width = newWidth + 'px';
+  });
+  
+  document.addEventListener('mouseup', () => {
+    isResizing = false;
+  });
